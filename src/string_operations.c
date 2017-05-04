9e3dc191d628d38e4eafc918631782959b467fe5
root <root@localhost.localdomain>
2017-05-04 17:35:47 -0500

    Added strchr function for wide character string

--- src/string_operations.c
+++ src/string_operations.c
@@ -3,6 +3,15 @@
 
 /* stash */
 
+wchar_t *my_wstrchr(wchar_t *ws, wchar_t wc){
+	while(*ws){
+		if(*ws == wc)
+		return ws;
+		++ws;
+	}
+	return NULL;
+}
+
 size_t my_wstrlen(const wchar_t *s){
 	const wchar_t *p = s;
 	while(*p)
